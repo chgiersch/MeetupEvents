8ed5c77aacc55d81d6148e2fbeea5ff8fc96bbd9
Chris Giersch <chgiersch@gmail.com>
2015-01-19 18:14:55 -0800

    working on comments

--- MeetupEvents/DetailViewController.m
+++ MeetupEvents/DetailViewController.m
@@ -9,11 +9,13 @@
 #import "DetailViewController.h"
 #import "WebViewController.h"
 
-@interface DetailViewController ()
+@interface DetailViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *eventNameLabel;
 @property (weak, nonatomic) IBOutlet UILabel *groupInfoLabel;
 @property (weak, nonatomic) IBOutlet UILabel *rsvpCountLabel;
 @property (weak, nonatomic) IBOutlet UITextView *eventTextField;
+
+@property NSArray *commentsArray;
 
 @end
 
@@ -32,11 +34,23 @@
 }
 
 
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"commentCell"];
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.commentsArray.count;
+}
+
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     WebViewController *wvc = segue.destinationViewController;
     wvc.linkToLoad = self.currentEvent[@"event_url"];
-    
+
 }
 
 
